bc75b41782cae1ed1d0f0f67d19a1fc93e7f23e5
Bryan Bergenholtz <116790009+YanRho@users.noreply.github.com>
2025-02-02 21:13:32 -0800

    resetting

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import NavigationBar from "@/components/navbar";
-
 export default function Home() {
   return (
     <>
-      <div className="h-screen text-white bg-black">
-        <NavigationBar/>
-      </div>
+       
     </>
-  );
+  )
 }
