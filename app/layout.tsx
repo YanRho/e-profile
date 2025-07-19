31629c27cf714914a17d54c4d4d719226ecf76ec
Bryan  Bergenholtz <brbergenholtz.19@gmail.com>
2025-07-19 00:53:57 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Bryan Rhoelle Bergenholtz",
+  title: "Bryan Bergenholtz | Software Dev",
   description: "My Portfolio",
 };
 
